c796335d6257837ff54867c5f5042d6a5df6b344
it21119330sas <87364240+it21119330sas@users.noreply.github.com>
2021-07-13 10:13:29 +0000

    completed tute 1

--- Tute01.c
+++ Tute01.c
@@ -4,7 +4,17 @@
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
+  int sum , mark1 , mark2 ;
+  float avg ;
+  printf("ENTER THE MARKS OF SUBJECT 1:"); 
+  scanf("%d", &mark1) ;
+  printf("ENTER MARKS OF THE SUBJECT 2: ");
+  scanf("%d", &mark2);
+  sum = mark1 + mark2 ;
+  avg=sum/2.00;
+  printf("THE AVERAGE OF THE TWO MARKS IS : %f",avg ); 
   
   return 0;
 }
